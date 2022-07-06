fa4df8eb38a7e71a213d418df70038818411b889
tarasavage <tarasuser9@gmail.com>
2022-07-06 23:37:58 +0300

    5 task & homework by Taras Panasiuk.

---
+++ IFlyable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework5
+{
+    internal interface IFlyable
+    {
+        void Fly();
+    }
+}
---
+++ Programmer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework5
+{
+    internal class Programmer : IDeveloper
+    {
+        string language;
+        public string Tool 
+        { 
+            get
+            { 
+                return language;
+            } 
+            set
+            { 
+                this.language = value;
+            }
+        }
+        public Programmer(string language)
+        {
+            this.language = language;
+        }
+        public void Destroy()
+        {
+            Console.WriteLine($"Programmer has destroyed class in {language}...");
+        }
+        public void Create()
+        {
+            Console.WriteLine($"Programmer has created something using {language}...");
+        }
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+        public override string ToString()
+        {
+            return String.Format($"This programmer writes code on {language}.");
+        }
+    }
+}
